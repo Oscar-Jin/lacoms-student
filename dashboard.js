e188ec366e4ec45dc5f3760706a91717a35cb005
Oscar-Jin <50736585+Oscar-Jin@users.noreply.github.com>
2019-12-26 14:12:46 +0900

    initial commit

---
+++ dashboard.js
@@ -0,0 +1,53 @@
+/* globals Chart:false, feather:false */
+
+(function () {
+  'use strict'
+
+  feather.replace()
+
+  // Graphs
+  var ctx = document.getElementById('myChart')
+  // eslint-disable-next-line no-unused-vars
+  var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: [
+        '８月',
+        '９月',
+        '１０月',
+        '１１月',
+        '１２月',
+        '１月',
+        '２月'
+      ],
+      datasets: [{
+        data: [
+          153,
+          213,
+          184,
+          240,
+          234,
+          240,
+          220
+        ],
+        lineTension: 0,
+        backgroundColor: 'transparent',
+        borderColor: '#007bff',
+        borderWidth: 4,
+        pointBackgroundColor: '#007bff'
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: false
+          }
+        }]
+      },
+      legend: {
+        display: false
+      }
+    }
+  })
+}())
